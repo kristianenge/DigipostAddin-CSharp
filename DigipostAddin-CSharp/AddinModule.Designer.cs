e647188eb219fcf0b03fd96a37ce541a2ebc31cf
Kristian Enge <kristian.ienge@gmail.com>
2015-07-30 15:17:53 +0200

    init

---
+++ DigipostAddin-CSharp/AddinModule.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DigipostAddin_CSharp
+{
+    partial class AddinModule
+    {
+        /// <summary>
+        /// Required by designer
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+ 
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+        /// <summary>
+        /// Required by designer support - do not modify
+        /// the following method
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddinModule));
+            this.adxRibbonTab1 = new AddinExpress.MSO.ADXRibbonTab(this.components);
+            this.adxRibbonGroup1 = new AddinExpress.MSO.ADXRibbonGroup(this.components);
+            this.adxSubjectBox = new AddinExpress.MSO.ADXRibbonEditBox(this.components);
+            this.adxSSNBox = new AddinExpress.MSO.ADXRibbonEditBox(this.components);
+            this.adxRibbonButton1 = new AddinExpress.MSO.ADXRibbonButton(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            // 
+            // adxRibbonTab1
+            // 
+            this.adxRibbonTab1.Caption = "Digipost";
+            this.adxRibbonTab1.Controls.Add(this.adxRibbonGroup1);
+            this.adxRibbonTab1.Id = "adxRibbonTab_f4f3a06680774e52a4c17af049390062";
+            this.adxRibbonTab1.InsertAfterIdMso = "TabInsert";
+            this.adxRibbonTab1.Ribbons = AddinExpress.MSO.ADXRibbons.msrWordDocument;
+            // 
+            // adxRibbonGroup1
+            // 
+            this.adxRibbonGroup1.Caption = " ";
+            this.adxRibbonGroup1.Controls.Add(this.adxSubjectBox);
+            this.adxRibbonGroup1.Controls.Add(this.adxSSNBox);
+            this.adxRibbonGroup1.Controls.Add(this.adxRibbonButton1);
+            this.adxRibbonGroup1.Id = "adxRibbonGroup_984f4cac3b87409aa2ca784f42958dfe";
+            this.adxRibbonGroup1.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.adxRibbonGroup1.Ribbons = AddinExpress.MSO.ADXRibbons.msrWordDocument;
+            // 
+            // adxSubjectBox
+            // 
+            this.adxSubjectBox.Caption = "Subject";
+            this.adxSubjectBox.Id = "adxRibbonEditBox_ed565c2a7915454f9dfd1247753c3da0";
+            this.adxSubjectBox.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.adxSubjectBox.Ribbons = AddinExpress.MSO.ADXRibbons.msrWordDocument;
+            // 
+            // adxSSNBox
+            // 
+            this.adxSSNBox.Caption = "SSN";
+            this.adxSSNBox.Id = "adxRibbonEditBox_9060d55fc6b64545ae5b400b13477be5";
+            this.adxSSNBox.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.adxSSNBox.Ribbons = AddinExpress.MSO.ADXRibbons.msrWordDocument;
+            // 
+            // adxRibbonButton1
+            // 
+            this.adxRibbonButton1.Caption = "Send";
+            this.adxRibbonButton1.Id = "adxRibbonButton_e4e24ad77f164b3389d1c9c1f28cbac1";
+            this.adxRibbonButton1.Image = 0;
+            this.adxRibbonButton1.ImageList = this.imageList1;
+            this.adxRibbonButton1.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.adxRibbonButton1.Ribbons = AddinExpress.MSO.ADXRibbons.msrWordDocument;
+            this.adxRibbonButton1.OnClick += new AddinExpress.MSO.ADXRibbonOnAction_EventHandler(this.adxRibbonButton1_OnClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "digipost_icon_128.png");
+            // 
+            // AddinModule
+            // 
+            this.AddinName = "DigipostAddin_CSharp";
+            this.SupportedApps = AddinExpress.MSO.ADXOfficeHostApp.ohaWord;
+
+        }
+        #endregion
+
+        private AddinExpress.MSO.ADXRibbonTab adxRibbonTab1;
+        private AddinExpress.MSO.ADXRibbonGroup adxRibbonGroup1;
+        private AddinExpress.MSO.ADXRibbonEditBox adxSubjectBox;
+        private AddinExpress.MSO.ADXRibbonEditBox adxSSNBox;
+        private AddinExpress.MSO.ADXRibbonButton adxRibbonButton1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ DigipostAddin-CSharp/AddinModule.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Forms;
+using AddinExpress.MSO;
+using Digipost.Api.Client;
+using System.Diagnostics;
+using Digipost.Api.Client.Api;
+using Microsoft.Office.Interop.Word;
+
+namespace DigipostAddin_CSharp
+{
+    /// <summary>
+    ///   Add-in Express Add-in Module
+    /// </summary>
+    [GuidAttribute("BEE06C06-7F4A-45D0-8716-01830A7DBEE5"), ProgId("DigipostAddin_CSharp.AddinModule")]
+    public partial class AddinModule : AddinExpress.MSO.ADXAddinModule
+    {
+        public AddinModule()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            InitializeComponent();
+            // Please add any initialization code to the AddinInitialize event handler
+        }
+ 
+        #region Add-in Express automatic code
+ 
+        // Required by Add-in Express - do not modify
+        // the methods within this region
+ 
+        public override System.ComponentModel.IContainer GetContainer()
+        {
+            if (components == null)
+                components = new System.ComponentModel.Container();
+            return components;
+        }
+ 
+        [ComRegisterFunctionAttribute]
+        public static void AddinRegister(Type t)
+        {
+            AddinExpress.MSO.ADXAddinModule.ADXRegister(t);
+        }
+ 
+        [ComUnregisterFunctionAttribute]
+        public static void AddinUnregister(Type t)
+        {
+            AddinExpress.MSO.ADXAddinModule.ADXUnregister(t);
+        }
+ 
+        public override void UninstallControls()
+        {
+            base.UninstallControls();
+        }
+
+        #endregion
+
+        public static new AddinModule CurrentInstance 
+        {
+            get
+            {
+                return AddinExpress.MSO.ADXAddinModule.CurrentInstance as AddinModule;
+            }
+        }
+
+        public Word._Application WordApp
+        {
+            get
+            {
+                return (HostApplication as Word._Application);
+            }
+        }
+
+        private void adxRibbonButton1_OnClick(object sender, IRibbonControl control, bool pressed)
+        {
+            const string SenderId = "779052"; //"106768801";
+            string Thumbprint = "84e492a972b7edc197a32d9e9c94ea27bd5ac4d9".ToUpper();
+            var config = new ClientConfig(SenderId)
+            {
+                ApiUrl = new Uri("https://api.digipost.no"),
+                Logger = (severity, konversasjonsId, metode, melding) =>
+                {
+                    Debug.WriteLine("{0} - {1} [{2}]",
+                        DateTime.Now,
+                        melding,
+                        konversasjonsId.GetValueOrDefault()
+                    );
+                }
+            };
+
+            //Logging.Initialize(config);
+            var client = new DigipostClient(config, Thumbprint);
+            
+            byte[] _contentBytes = null;
+            var curdoc = WordApp.ActiveDocument;
+            var range = curdoc.Content;
+            var newDocument = new Word.Document();
+
+            object destFilename = "E:\\Test\\test.pdf";
+
+            object missing = Type.Missing;
+            curdoc.SaveAs( ref destFilename,  WdExportFormat.wdExportFormatPDF, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+            
+            
+            var fullname = curdoc.FullName;
+
+            
+            //for(int i =1; i< range.PageSetup) //loop through pages
+            var str = "";
+            for (int i = 1; i < range.Paragraphs.Count; i++)
+            {
+                str = str + range.Paragraphs.Item(i).Range.Text;
+            }
+
+            _contentBytes = System.Text.Encoding.UTF8.GetBytes(str);
+            
+
+            var recipient = new Digipost.Api.Client.Domain.Recipient(Digipost.Api.Client.Domain.Enums.IdentificationChoice.PersonalidentificationNumber, adxSSNBox.Text.Trim());
+            var document = new Digipost.Api.Client.Domain.Document(adxSubjectBox.Text, "pdf",(string) destFilename);
+
+            var message = new Digipost.Api.Client.Domain.Message(recipient, document);
+            client.SendMessage(message);
+
+            Marshal.ReleaseComObject(range);
+            Marshal.ReleaseComObject(curdoc);
+
+            
+
+        }
+        private void Test()
+        {
+            object visible = true;
+            object readOnly = true;
+            object sourceFilename = "";
+            string targetFilename = "";
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+
+            Document doc = wordApp.Documents.Open(ref sourceFilename, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing,
+                                                  ref missing, ref missing, ref missing, ref missing, ref visible, ref missing, ref missing,
+                                                  ref missing, ref missing);
+            
+            doc.ExportAsFixedFormat(targetFilename, WdExportFormat.wdExportFormatPDF, false, WdExportOptimizeFor.wdExportOptimizeForOnScreen,
+                                    WdExportRange.wdExportAllDocument, 0, 0, WdExportItem.wdExportDocumentContent, false, false,
+                                    WdExportCreateBookmarks.wdExportCreateNoBookmarks, false, false, false, ref missing);
+
+            object saveChanges = false;
+            doc.Close(ref saveChanges, ref missing, ref missing);
+            wordApp.Quit(ref saveChanges, ref missing, ref missing);
+        }
+
+        static object missing = Type.Missing;
+    }
+    
+}
+
